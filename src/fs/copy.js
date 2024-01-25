97ca57bf6dbb349500fd555cfe6ae3c8a3fa4741
NikFreeman <Classic76@tut.by>
2024-01-25 16:44:21 +0300

    feat: complete copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import * as fs from 'node:fs/promises';
+import * as url from 'node:url';
+import * as path from 'node:path'
+
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here 
+  const __DIRNAME = url.fileURLToPath(new URL('.', import.meta.url));
+  const SOURCE_FOLDER = 'files';
+  const DESTINATION_FOLDER = 'files_copy';
+  try {
+    try {
+    await fs.access(path.join(__DIRNAME,SOURCE_FOLDER));
+    await fs.mkdir(path.join(__DIRNAME,DESTINATION_FOLDER));
+    let files = await fs.readdir(path.join(__DIRNAME, SOURCE_FOLDER));
+      for (let file of files){
+        await fs.copyFile(path.join(__DIRNAME,SOURCE_FOLDER,file),path.join(__DIRNAME,DESTINATION_FOLDER,file), fs.COPYFILE_EXCL);
+      }      
+    }
+    catch {      
+      throw new Error('FS operation failed');
+    }    
+  }
+  catch (e) {
+    console.error(`${e.name}: ${e.message}`);
+  }
 };
 
 await copy();
