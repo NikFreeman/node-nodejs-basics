5c28be9c2d9faa3d71489f6a06aed8145a0c120f
NikFreeman <Classic76@tut.by>
2024-01-26 17:43:21 +0300

    feat: list task complete

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import * as fs from 'node:fs/promises';
+import * as url from 'node:url';
+import * as path from 'node:path';
+
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    const __DIRNAME = url.fileURLToPath(new URL('.', import.meta.url));
+    const SOURCE_FOLDER = 'filess';
+    
+    try {
+        try{
+          const files = await fs.readdir(path.join(__DIRNAME, SOURCE_FOLDER));        
+          for (let file of files){
+            console.log(file);            
+          }      
+        }     
+        catch{
+          throw new Error('FS operation failed'); 
+        }
+    }
+    catch (e) {
+      console.error(`${e.name}: ${e.message}`);
+    } 
 };
 
 await list();
