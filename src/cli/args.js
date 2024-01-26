6e61865a8bd9022c52447761c89a6f66e01d4035
NikFreeman <Classic76@tut.by>
2024-01-26 20:18:48 +0300

    feat: args task complete

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
     // Write your code here 
+    let result ='';
+    process.argv.forEach((arg,index) => {
+        if (index > 1){
+        (arg.slice(0,2) == '--')? result += `${arg.slice(2)} is `: result += `${arg}, `;
+    }
+    } );
+    console.log(result.slice(0,-2));
 };
 
 parseArgs();
