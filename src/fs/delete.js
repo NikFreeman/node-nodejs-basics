bcdec9fb67d8862fe17411bcabe61ef04a9eaa3a
NikFreeman <Classic76@tut.by>
2024-01-26 17:30:50 +0300

    feat: delete task complete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import * as fs from 'node:fs/promises';
+import * as url from 'node:url';
+import * as path from 'node:path';
+
+
 const remove = async () => {
     // Write your code here 
+  const __DIRNAME = url.fileURLToPath(new URL('.', import.meta.url));
+  const SOURCE_FOLDER = 'files';
+  const DELETE_FILE='fileToRemove.txt';
+  
+  try {
+    try {
+      await fs.rm(path.join(__DIRNAME,SOURCE_FOLDER,DELETE_FILE))
+    }
+    catch{
+      throw new Error('FS operation failed');
+    }
+  }
+  catch (e) {
+    console.error(`${e.name}: ${e.message}`);
+  }
+
 };
 
 await remove();
