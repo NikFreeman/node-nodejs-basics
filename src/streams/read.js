54543448e1c03915358e68b9a0377a2c64d04558
NikFreeman <Classic76@tut.by>
2024-01-27 23:24:00 +0300

    feat: streamsRead task complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import * as fs from "node:fs/promises"
+import { stdout } from 'node:process';
+import * as url from 'node:url';
+import * as path from 'node:path';
+
 const read = async () => {
     // Write your code here 
+    const __DIRNAME = url.fileURLToPath(new URL('.', import.meta.url));
+    const FILE_READ = 'fileToRead.txt';
+    const SOURCE_FOLDER = 'files';
+  
+    const fd =await fs.open(path.join(__DIRNAME, SOURCE_FOLDER, FILE_READ));
+    const readableStream = fd.createReadStream();
+    readableStream.pipe(stdout);
 };
 
 await read();
