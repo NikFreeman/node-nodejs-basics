65436d1e3b1a20ca27759ab7c2cb1769aad97186
NikFreeman <Classic76@tut.by>
2024-01-27 22:32:24 +0300

    feat: calcHash task complete

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import * as fs from "node:fs/promises"
+import * as crypto from "node:crypto"
+import { stdout } from 'node:process';
+import * as url from 'node:url';
+import * as path from 'node:path';
+
+
 const calculateHash = async () => {
     // Write your code here 
+  const __DIRNAME = url.fileURLToPath(new URL('.', import.meta.url));
+  const FILE_CALCULATE = 'fileToCalculateHashFor.txt';
+  const SOURCE_FOLDER = 'files';
+
+  const fd =await  fs.open(path.join(__DIRNAME, SOURCE_FOLDER,  FILE_CALCULATE));
+  const readableStream = fd.createReadStream();
+  const hash = crypto.createHash('sha256');
+
+  readableStream.pipe(hash).setEncoding('hex').pipe(stdout);
+
 };
 
 await calculateHash();
